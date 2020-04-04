5f130314c4be793614fad0342c660f4bd951d8c7
Woo_Sj <wsj94472681@gmail.com>
2020-04-04 23:55:23 +0900

    3주차 과제 완

--- STL_Class(2020)/main.cpp
+++ STL_Class(2020)/main.cpp
@@ -1,51 +1,25 @@
 #include <iostream> 
-#include <string>
-#include <random>
-#include <algorithm>
-#include <fstream>
-
+#include <memory>
 using namespace std;
 
 class Dog {
-	string name; // 이름 - 15글자까지만 허용
-	int age; // 나이
-	int id; // 생성 시 결정되는 고유의 숫자로 된 id
+	int n;
 public:
-	Dog() {
-		name = { NULL };
-		age = 0;
-		id = 0;
+	// 생성과 소멸 관찰용
+	Dog() { cout << "생성" << endl; }
+	~Dog() { cout << "소멸" << endl; }
+	void show() {
+		cout << "Dog입니다만" << endl;
 	}
-
-	Dog(string name, int age, int id) : name{ name }, age{ age }, id(id) {
-	}
-
-	string getname() {
-		return this->name;
-	}
-	operator <(const Dog& a, const Dog& b) {
-		if(a.)
-	}
-
-	friend ostream& operator<<(ostream&, const Dog&);
 };
 
-ostream& operator<<(ostream& os, const Dog& dog) {
-	os << dog.name << ' ' << dog.age << ' ' << dog.id;
-	return os;
-}
-
-int main() {
-	ifstream in("Dog만마리", ios::binary);
-
-	Dog data[10000];
-
-	in.read((char*)data, sizeof(Dog) * 10000);
-
-	sort(begin(data), end(data), [](Dog& a, Dog& b) {
-		return a > b;
-		});
-
-	for (const Dog& dog : data)
-		cout << dog << endl;
+int main()
+{
+	// 관찰이 쉽게 코드를 지역으로 나눴다
+	{
+		// 객체 한 개 - 이렇게 unique_ptr를 생성하는 것이 더 좋다
+		auto dog = make_unique<Dog>();
+	}
+	// 객체 여러 개
+	unique_ptr<Dog[]> dogs{ new Dog[3] };
 }
